a8671daaddd72f3baf6abf9a25492c1faa1c1b69
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2023-04-05 00:52:27 +0530

    first commit

---
+++ Java/Codes/src/App.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class App{
+
+    class Base{
+        private int a,b;
+        public void get(){
+            System.out.println("Enter 2 Numbers ");
+        }
+        public void set(){}
+    }
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
