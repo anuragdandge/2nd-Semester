d2a253695333283ac06eb1ad569e61db14d3968a
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2023-04-05 15:04:51 +0530

    One Bash program for Addition added

---
+++ OS(Bash)/Books/Bash/Addition.sh
@@ -0,0 +1,27 @@
+# 1st Approach
+num1=10
+num2=20
+echo "Addition of $num1+$num2 = $(expr $num1 + $num2 )"
+
+
+# 2nd Approach
+num3=30
+num4=40
+sum=`expr $num3 + $num4`
+echo "Addition of $num3+$num4 = $sum"
+
+
+#3rd Approach
+
+num5=50
+num6=60
+sum=$(($num5 + $num6))
+echo "Addition of $num5+$num6 = $sum"
+
+
+#4th Approach
+
+read -p "Enter first Num :" a
+read -p "Enter second Num :" b
+echo "Addition of $a+$b = $(($a + $b))"
+
